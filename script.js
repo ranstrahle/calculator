9074645aba8853ec68e55b59a7acb8c6fcd8a98e
ran <randallstrahle@gmail.com>
2023-01-30 23:29:57 -0800

    Addclear and delete functionalit

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+ let operatorRan = false;
+ 
  function attachEventToNumber() {
         const numbers = document.querySelectorAll(".number")
         numbers.forEach(number => {
@@ -7,22 +9,58 @@
     
 attachEventToNumber();
 
-function changeNumber(number) {
-    const displayBottom = document.querySelector(".display-bottom");
-    if(displayBottom.innerHTML == "0") {
-        displayBottom.innerHTML = number.innerHTML;
-    } else {
-        displayBottom.innerHTML += number.innerHTML;
-    }
-}
-
 function attachPlusEvent() {
     document.querySelector(".addition").addEventListener("click", (e) => add());
 }
 
 attachPlusEvent();
 
+function attachClearEntryEvent() {
+    document.querySelector(".clear-entry").addEventListener("click", (e) => {
+        document.querySelector(".display-bottom").innerHTML = "0";
+    });
+}
+
+attachClearEntryEvent();
+
+function attachClearEvent() {
+document.querySelector(".clear").addEventListener("click", (e) => {
+    document.querySelector(".display-bottom").innerHTML = "0";
+    document.querySelector(".display-top").innerHTML = "empty";
+});
+}
+
+attachClearEvent();
+
+function attachDeleteEvent() {
+    document.querySelector(".delete").addEventListener("click", (e) => {
+        let displayBottom = document.querySelector(".display-bottom");
+        if(displayBottom.innerHTML.length == 1){
+            displayBottom.innerHTML = "0";
+        } else {
+            displayBottom.innerHTML = displayBottom.innerHTML.slice(0, -1);
+        }
+    });
+    }
+
+attachDeleteEvent();
+
+function operatorHasBeenRun() {
+    let operatorRan = true;
+}
+
+function changeNumber(number) {
+    const displayBottom = document.querySelector(".display-bottom");
+    if(displayBottom.innerHTML == "0" || operatorRan == true) {
+        displayBottom.innerHTML = number.innerHTML;
+        operatorRan = false;
+    } else {
+        displayBottom.innerHTML += number.innerHTML;
+    }
+}
+
 function add() {
+    operatorRan = true;
     const displayTop = document.querySelector(".display-top");
     const displayBottom = document.querySelector(".display-bottom");
     let displayTopValue = displayTop.innerHTML.replace(/\W/g, "");
@@ -32,6 +70,7 @@ function add() {
     } else 
        displayTop.innerHTML = displayBottom.innerHTML + " +";
     }
+
 function subtract(x,y) {
     return x - y;
 }
